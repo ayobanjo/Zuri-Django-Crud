41e9d518417d7bd9c8e4874f17acb4617249993c
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-06-25 20:27:18 +0100

    templates added, urls updated

--- I4G003024QA8/blog/views.py
+++ I4G003024QA8/blog/views.py
@@ -1,3 +1,32 @@
+from pyexpat import model
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+
+from I4G003024QA8.blog.models import Post
+
+
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model: Post
+    fields = '__all__'
+    success_url: reverse_lazy("blog:all")
+class PostDetailView(DetailView):
+    model = Post
+class PostUpdateView(UpdateView):
+    model =Post
+    fields = '__all__'
+    success_url: reverse_lazy("blog:all")
+class PostDeleteView(UpdateView):
+    model =Post
+    fields = '__all__'
+    success_url: reverse_lazy("blog:all")
+
 
 # Create your views here.
